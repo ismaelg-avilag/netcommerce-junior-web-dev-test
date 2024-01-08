5ffe52ad8dc89bf81c79e3ad4c2726408f73e49a
Ismael Avila <ismaelg.avilag@gmail.com>
2024-01-07 19:13:38 -0600

    Creación de migraciones y modelos
    - Se crearon las migraciones de las tablas de Company y Task
    - Se crearon los modelos de las clases Company y Task: se agregaron los $fillable y las relaciones entre estas clases.

---
+++ app/Models/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Company;
+use App\Models\User;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'user_id',
+        'is_completed',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
