82ac00856934745874fd9b8ee5f568220638177f
Ismael Avila <ismaelg.avilag@gmail.com>
2024-01-09 15:05:27 -0600

    Endpint `tasks/create`
    - Se creó la ruta para registrar tareas
    - La request recibe: company_id, name, description y user_id
    - En la respuesta se envía la información de la tarea creada.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $task = Task::create($request->all());
+
+        return response()->json([
+            "id" => $task->id,
+            "name" => $task->name,
+            "description" => $task->description,
+            "user" => $task->user->name,
+            "company" => [
+                "id" => $task->company->id,
+                "name" => $task->company->name,
+            ]
+        ], 201);
+    }
 }
