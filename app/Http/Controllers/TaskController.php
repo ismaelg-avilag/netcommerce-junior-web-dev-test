0c6dcb23b219dbc8b1dabe0a2dd5fa0ec3a7c958
Ismael Avila <ismaelg.avilag@gmail.com>
2024-01-09 15:43:05 -0600

    Validaciones para creación de tareas
    - Se añadieron reglas de validación para la creación de tareas
    - Las reglas verifican que todos los datos para crear una tarea son requeridos y el user_id y company_id existen en la base de datos
    - También se verifica que un usuario no pueda crear más de 5 tareas sin completarse.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,6 +9,19 @@ class TaskController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string|min:1|max:255',
+            'description' => 'required|string|min:1|max:255',
+            'user_id' => 'required|exists:App\Models\User,id',
+            'company_id' => 'required|exists:App\Models\Company,id',
+        ]);
+
+        if(Task::where('user_id', $request->user_id)->where('is_completed', false)->count() > 5) {
+            return response()->json([
+                "message" => "You can't have more than 5 uncompleted tasks"
+            ], 400);
+        }
+
         $task = Task::create($request->all());
 
         return response()->json([
